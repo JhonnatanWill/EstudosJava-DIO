91591c27a28a9c99b7aff1eec0ab8e4934120403
JhonnatanWill <jhonnatanwill@gmai.com>
2023-03-01 12:36:40 -0300

    Primeira aula!

---
+++ Java/Projeto001/src/MinhaClasse.java
@@ -0,0 +1,12 @@
+package Projeto001.src;
+
+public class MinhaClasse {
+    
+    public static void main (String [] args) {
+        System.out.print ( "Olá, meu nome é Jhonnatan Will!");
+
+    }
+
+}
+
+
