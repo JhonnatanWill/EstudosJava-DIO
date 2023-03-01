d4c9d78b916d4e6b25a913307595de0f15a1fe5e
JhonnatanWill <jhonnatanwill@gmai.com>
2023-03-01 17:40:51 -0300

    Testando alguns tipos de variáveis

--- Java/Projeto001AulasSintaxeJava/README.md
+++ Java/Projeto001AulasSintaxeJava/README.md
@@ -1,6 +1,10 @@
-# Sobre as Aula
+# Sobre as Atualizações
 
-1ª Aula: Foi demonstrado o início básico sobre a anatomia de classes.
-2ª Aula: Noções sobre entradas de nomes apropiados para a criação de uma variável.
-3ª Aula: Como declarar variáveis e métodos e definir atribuição e valor.
-4ª Aula: Entendendo um pouco sobre indentação.
+1ª - Foi demonstrado o início básico sobre a anatomia de classes.
+2ª - Noções sobre entradas de nomes apropiados para a criação de uma variável.
+3ª - Como declarar variáveis e métodos e como definir atribuição e valor.
+4ª - Entendendo um pouco sobre indentação.
+5ª - Como organizar os projetos em pacotes.
+6ª - Aprofundando os tipos e convenções de entradas da segunda aula.
+7º - Criando um repositório no GitHub e sicronizando com o VS Code.
+8ª - Testando alguns tipos de variáveis.
---
+++ Java/Projeto001AulasSintaxeJava/src/TipoVariaveis.java
@@ -0,0 +1,32 @@
+package Projeto001AulasSintaxeJava.src;
+
+public class TipoVariaveis {
+    
+    public static void main(String [] args) throws Exception{
+    byte idade = 27;
+    short ano = 2023;
+    int cep = 53401150;
+    long cpf = 10484210282L; //Tem que ser finalizado com L
+    float pi = 3.14F; //Tem que ser finalizado com F
+    double salarioMinimo = 2500.21;
+
+    short numeroCurto = 1;
+    int numeroNormal = numeroCurto;
+    short numeroCurto2 = (short) numeroNormal;
+
+    int numero = 5;
+    numero = 10;
+    System.out.println(numero);
+
+    double VALORDEPI = 3.14;
+    VALORDEPI = 10;
+    System.out.println(VALORDEPI);
+
+    /*
+    final double VALORDEPIDOIS = 3.14;
+    VALORDEPIDOIS = 10; // Na hora de rodar acusará error, pois a variável anterior alem de estar toda em maiúsculo, começa com a preposição "final".
+    System.out.println(VALORDEPIDOIS);
+    */
+
+    }
+}
