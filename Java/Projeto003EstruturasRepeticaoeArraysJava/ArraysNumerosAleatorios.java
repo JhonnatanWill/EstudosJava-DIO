29a89a52b22d9f7f6eeee9edd1990e46350144d4
JhonnatanWill <jhonnatanwill@gmai.com>
2023-03-23 18:52:34 -0300

    Arrays = Números Aleatórios

---
+++ Java/Projeto003EstruturasRepeticaoeArraysJava/ArraysNumerosAleatorios.java
@@ -0,0 +1,28 @@
+package Projeto003EstruturasRepeticaoeArraysJava;
+
+import java.util.Random;
+
+public class ArraysNumerosAleatorios {
+    
+    public static void main(String[] args){
+
+        Random random = new Random();
+
+        int[] numerosAleaorios = new int[20];
+
+        for(int i = 0; i < numerosAleaorios.length; i++){
+            int numero = random.nextInt(100);
+            numerosAleaorios[i] = numero;
+        }
+
+        System.out.print("Números aleatórios: ");
+        for (int numero : numerosAleaorios) {
+            System.out.print(numero + " ");
+        }
+
+        System.out.print("\nSucessores dos números aleatórios: ");
+        for (int numero : numerosAleaorios) {
+            System.out.print((numero+1) + " ");
+        }
+    }
+}
