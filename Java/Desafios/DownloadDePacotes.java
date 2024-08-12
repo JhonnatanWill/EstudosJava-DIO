a07cdea3b1a264b93cc37512b35872b4c1de2729
JhonnatanWill <jhonnatanwill@gmai.com>
2024-08-12 11:31:39 -0300

    Fechando o leitor

--- Java/Desafios/DownloadDePacotes.java
+++ Java/Desafios/DownloadDePacotes.java
@@ -14,7 +14,7 @@ public class DownloadDePacotes {
     public static void main(String[] args) {
 
         Scanner leitor = new Scanner(System.in);
-
+        
         System.out.println("Quantos porcento?");
         int tamanho = leitor.nextInt();
 
