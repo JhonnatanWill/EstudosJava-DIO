f940292cc07f89c1922f3e80f0dc163e57342bbc
JhonnatanWill <jhonnatanwill@gmai.com>
2024-08-27 17:58:17 -0300

    Desafio, conta bancária!

---
+++ FormaçãoJavaDeveloper/Desafios/src/ContaBancoTerminal.java
@@ -0,0 +1,58 @@
+package Desafios.src;
+
+/*
+Desafio
+Vamos exercitar todo o conteúdo apresentado no módulo de Sintaxe codificando 
+o seguinte cenário.
+
+Crie o projeto ContaBanco que receberá dados via terminal contendo as 
+características de conta em banco conforme atributos abaixo:
+Dentro do projeto, crie a classe ContaTerminal.java para realizar toda 
+a codificação do nosso programa.
+
+Revise sobre regras de declaração de variáveis
+Atributo	Tipo	Exemplo
+Numero	Inteiro	1021
+Agencia	Texto	067-8
+Nome Cliente	Texto	MARIO ANDRADE
+Saldo	Decimal	237.48
+
+Conhecer e importar a classe Scanner
+Exibir as mensagens para o nosso usuário
+Obter pela classe scanner os valover digitados no  terminal
+Exibir a mensagem conta criada
+
+Entrada
+Permita que os dados sejam inseridos via terminal sendo que o usuário 
+receberá a mensagem de qual informação será solicitada, exemplo:
+Programa: "Por favor, digite o número da Agência !"
+Usuário: 1021 (depois ENTER para o próximo campo)
+
+Saída
+Depois de todas as informações terem sido inseridas, o sistema deverá exibir 
+a seguinte mensagem:
+"Olá [Nome Cliente], obrigado por criar uma conta em nosso banco, sua agência 
+é [Agencia], conta [Numero] e seu saldo [Saldo] já está disponível para saque".
+
+Os campos em [ ] devem ser alterados pelas informações que forem inseridas 
+pelos usuários.
+*/
+
+import java.util.Scanner;
+
+public class ContaBancoTerminal {
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+	 	Scanner scanner = new Scanner (System.in);
+ 
+		int numero;
+		String agencia = "067-8";
+		String nomeCliente = "JHONNATAN WILLIAN";
+		double saldo = 237.48;
+
+		System.out.println("Por favor, digite o número da Agência!");
+ 		numero = scanner.nextInt();  
+ 
+ 		System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque!");
+	}
+}
